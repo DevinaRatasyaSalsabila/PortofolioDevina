d9442b31752d0b60d5a8d0ee36c5f4ab0b7b6290
DevinaRatasyaSalsabila <devinaratasya2812@gmail.com>
2024-11-04 16:00:41 +0700

    Update PortofolioController.php

--- app/Http/Controllers/PortofolioController.php
+++ app/Http/Controllers/PortofolioController.php
@@ -22,7 +22,7 @@ class PortofolioController extends Controller
     public function indexLanding()
     {
         $portofolio = portofolio::All();
-        return view('index', [
+        return view('/', [
             'portofolio' => $portofolio
         ]);
     }
